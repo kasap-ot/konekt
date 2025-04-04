dfffbb81e4599ce0a546b281c4c8927eedf4a92b
Petar Kasapinov <pkasapinov@otcfin.com>
2025-04-04 18:07:52 +0200

    Extract component - Empty State

--- app/routes/list-events.tsx
+++ app/routes/list-events.tsx
@@ -6,6 +6,7 @@ import { Colors } from '../../styles/globalStyles';
 import { Event } from 'types';
 import Header from '../../components/Header';
 import EventListItem from 'components/EventListItem';
+import EmptyState from '../../components/EmpyState';
 
 
 const ListEventsPage: React.FC = () => {
@@ -13,34 +14,29 @@ const ListEventsPage: React.FC = () => {
   const router = useRouter();
   const { category } = useLocalSearchParams<{ category?: string }>();
 
-  const handleEventPress = (eventId: string) => {
+  function handleEventPress(eventId: string): void {
     router.push(`/routes/event?id=${eventId}`);
   };
 
-  const filteredEvents = category 
-    ? events.filter((event: Event) => event.category === category) 
+  const filteredEvents = category
+    ? events.filter((event: Event) => event.category === category)
     : events;
 
-  
   const headerText = category || 'Events';
 
-  const renderEventItem = ({ item }: { item: Event }) => (
-    <EventListItem event={item} onPress={() => handleEventPress(item.$id)}/>
-  );
+  function renderEventItem({ item }: { item: Event }) {
+    return (<EventListItem event={item} onPress={() => handleEventPress(item.$id)} />);
+  }
 
   return (
     <View style={styles.container}>
-      <Header title={headerText}/>
+      <Header title={headerText} />
 
       <FlatList
         data={filteredEvents}
         keyExtractor={(item: Event) => item.$id}
         renderItem={renderEventItem}
-        ListEmptyComponent={
-          <View style={styles.emptyContainer}>
-            <Text style={styles.emptyText}>No events found in this category.</Text>
-          </View>
-        }
+        ListEmptyComponent={<EmptyState message='No events found for this category' />}
       />
     </View>
   );
@@ -50,28 +46,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.background.primary,
-  },
-  image: {
-    width: 100,
-    height: 100,
-  },
-  button: {
-    backgroundColor: Colors.text.primary,
-    paddingVertical: 12,
-    paddingHorizontal: 15,
-    borderRadius: 8,
-    alignItems: 'center',
-    margin: 10,
-  },
-  emptyContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 50,
-  },
-  emptyText: {
-    color: Colors.text.primary,
-    fontSize: 18,
   },
 });
 

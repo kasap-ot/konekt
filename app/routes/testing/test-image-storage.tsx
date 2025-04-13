419634a7fb4af66be58dcb0177de1f1b1c7e85b5
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2025-04-13 13:46:39 +0200

    Implement test reading image from Appwrite

--- app/routes/testing/test-image-storage.tsx
+++ app/routes/testing/test-image-storage.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, Image } from 'react-native';
 import { Client, Storage } from 'react-native-appwrite';
 
 const endpoint = 'https://cloud.appwrite.io/v1';
@@ -8,30 +8,36 @@ const bucketId = '67f7e35b000dfa9a3674';
 const fileId = '67f7e395001bc9365d06';
 
 const client = new Client();
-client
-    .setEndpoint(endpoint)
-    .setProject(projectId);
-
+client.setEndpoint(endpoint).setProject(projectId);
 export const storage = new Storage(client);
 
-
 export default function TestImageAppwrite() {
-    useEffect(() => {
-        async function fetchFile() {
-            try {
-                const result = await storage.getFile(bucketId, fileId);
-                console.log('File content:', result);
-            } catch (error) {
-                console.error('Error fetching file:', error);
-            }
-        };
+  const [imageUri, setImageUri] = useState<string | null>(null);
 
-        fetchFile();
-    }, []);
+  useEffect(() => {
+    const fetchFile = () => {
+      try {
+        const result = storage.getFileView(bucketId, fileId);
+        setImageUri(result.toString());
+        console.log('Image URL:', result.toString());
+      } catch (error) {
+        console.error('Error fetching file:', error);
+      }
+    };
 
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Check console for file content</Text>
-        </View>
-    );
+    fetchFile();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Check console for file content</Text>
+      {imageUri && (
+        <Image
+          source={{ uri: imageUri }}
+          style={{ width: 200, height: 200, borderRadius: 10 }}
+          resizeMode="contain"
+        />
+      )}
+    </View>
+  );
 }
